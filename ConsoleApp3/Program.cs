ae4c26195e214232d8194f06c96490e947789b1f
Juanfercero <162922620+Juanfercero@users.noreply.github.com>
2024-09-19 08:37:00 -0300

    Primer Commit

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+//act 1
+int num = 0;
+Console.WriteLine(num);
+num = +5;
+Console.WriteLine(num);
