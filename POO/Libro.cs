8e72cfa429892d0f5a0c3240ad72f6abe6deb6a8
Juanfercero <162922620+Juanfercero@users.noreply.github.com>
2024-10-09 13:45:49 -0300

    POO Act 1 Resuelto
    Mario Chuzo

---
+++ POO/Libro.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Libro
+    {
+        public string Titulo = "";
+        public string Autor = "";
+        public int CantCopias = 0;
+        public int CantPrestamo = 0;
+
+        public Libro(string titulo, string autor, int cantCopias, int cantPrestamo)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            CantCopias = cantCopias;
+            CantPrestamo = cantPrestamo;
+        }
+
+        public void Prestar()
+        {
+            if (CantCopias >= 1)
+            {
+                CantPrestamo++;
+                CantCopias--;
+                Console.WriteLine($"Se ha prestado una copia del libro {Titulo}");
+            }
+            else
+            {
+                Console.WriteLine($"El libro {Titulo} no tiene stock");
+            }
+        }
+
+        public void Devolver()
+        {
+            if (CantPrestamo >= 1)
+            {
+                CantPrestamo--;
+                CantCopias++;
+                Console.WriteLine($"Se ha devuelto una copia del libro {Titulo}");
+            }
+            else
+            {
+                Console.WriteLine($"El libro {Titulo} no tiene copias que devolver");
+            }
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine($"*");
+            Console.WriteLine($"Título: {Titulo}");
+            Console.WriteLine($"Autor: {Autor}");
+            Console.WriteLine($"Stock: {CantCopias}");
+            Console.WriteLine($"Copias Prestadas: {CantPrestamo}");
+            Console.WriteLine($"*");
+        }
+    }
+}
---
+++ POO/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POO;
+
+Libro libro1 = new Libro("Peter Punk", "J. Matthew del Barrio", 2, 0);
+Libro libro2 = new Libro("Sherlock Home", "A. Conan Detective", 1, 1);
+Libro libro3 = new Libro("La Madrina", "Mario Chuzo", 3, 0);
+
+libro1.MostrarDatos();
+
+libro1.Prestar();
+libro1.Prestar();
+libro1.Prestar();
+
+libro1.MostrarDatos();
+
+libro1.Devolver();
+libro1.Devolver();
+libro1.Devolver();
+
+libro1.MostrarDatos();
+
+libro2.MostrarDatos();
+
+libro2.Prestar();
+libro2.Prestar();
+
+libro2.MostrarDatos();
+
+libro2.Devolver();
+libro2.Devolver();
+libro2.Devolver();
+
+libro2.MostrarDatos();
